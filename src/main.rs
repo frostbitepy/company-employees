42bdc31638a88875280cb25e20e856e248be7eb2
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2024-02-26 15:48:52 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// Usin a hash map and vectors, create a text interface to allow a user
+// to add employee names to a department in a company. For example, 
+// "Add Sally to Engineering" or "Add Amir to Sales". Then let the user
+// retrieve a list of all people in a department or all people in the 
+// company by department, sorted alphabetically.
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut _company: HashMap<String,Vec<String>> = HashMap::new();
+    let mut v: Vec<String> = vec!["fran".to_string(), "pedro".to_string(), "pepe".to_string()];
+    let nombre = String::from("juan"); 
+    let mut v2 = add_employee(nombre, &mut v);
+
+    println!("{:#?}", v);
+}
+
+fn add_employee(name: String, listado: &mut Vec<String>) -> &Vec<String> {
+    listado.push(name);
+    listado
+}
